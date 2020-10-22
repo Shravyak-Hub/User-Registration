857a261cf9dea2ef6d59922d306d83f67389826e
Shravyak-Hub <shravyak156@gmail.com>
2020-10-22 14:41:10 +0530

    Modified file: UserRegistrationMain.java, added code to get and validate user's email

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class UserRegistrationMain {
 
     public static final String NAME_REGEX = "^[A-Z][a-z]{2,}$";
+    public static final String EMAIL_REGEX = "^([a-z0-9]+[-._+]?[a-z0-9]+)+@[a-z0-9-]+.[a-z]{2,3}.[a-z]{2,3}$";
 
     Scanner scanner = new Scanner(System.in);
 
@@ -10,20 +11,34 @@ public class UserRegistrationMain {
         UserRegistrationMain userRegistrationMain = new UserRegistrationMain();
         userRegistrationMain.validateFirstName();
         userRegistrationMain.validateLastName();
+        userRegistrationMain.validateEmail();
     }
 
     public void validateFirstName() {
         System.out.println("Enter first name: ");
         String firstName = scanner.nextLine();
-        if(!firstName.matches(NAME_REGEX))
+        if(firstName.matches(NAME_REGEX))
+            System.out.println("First name is valid");
+        else
             System.out.println("First name is invalid");
     }
 
     public void validateLastName() {
         System.out.println("Enter last name: ");
         String lastName = scanner.nextLine();
-        if(!lastName.matches(NAME_REGEX))
+        if(lastName.matches(NAME_REGEX))
+            System.out.println("Last name is valid");
+        else
             System.out.println("Last name is invalid");
+    }
+
+    public void validateEmail() {
+        System.out.println("Enter email: ");
+        String email = scanner.nextLine();
+        if(email.matches(EMAIL_REGEX))
+            System.out.println("Email is valid");
+        else
+            System.out.println("Email is invalid");
     }
 
 }
